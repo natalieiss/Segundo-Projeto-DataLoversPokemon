6df4d402872193e033bad697ea103e3c1a542064
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-02-21 17:52:06 -0300

    Updates

--- data.js
+++ data.js
@@ -1,6 +1,20 @@
-// estas funciones son de ejemplo
+export const showPokemons = (pokemonsList) => pokemonsList.slice(0, 251)
 
-export const pokemonNameData = () => {
+
+//Filtrar Pokémons por tipo através do select
+export const filterByType = (data, selectFilter) =>
+  data.filter((pokemon) => pokemon.type.includes(selectFilter))
+
+//Cálculo
+export const percentType = (data, filterCalc) => {
+  const calcPoke = data.filter((pokemon) => pokemon.type.includes(filterCalc))
+  return Math.round((calcPoke.length / data.length) * 100)
+}
+
+
+/* estas funciones son de ejemplo
+
+export const pokemonData = () => {
   return 'example';
 };
 
@@ -8,12 +22,34 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
-// export const showPokemons = (pokemonsList) => pokemonsList.slice(0, 251);
-// // Filtrar Pokémons por nome através do input text
-// export const filterByName = (pokemonsList, pokemonsName) => {
-//   const lowerCaseName = pokemonsName.toLowerCase();
-//   return pokemonsList
-//     .filter((pokemon) => pokemon.name.toLowerCase().startsWith(lowerCaseName))
-//     .slice(0, 251);
-// };
+// Mostrar quantidade de Pokémons que quisermos selecionar
+export const showPokemons = (pokemonsList) => pokemonsList.slice(0, 251);
 
+// Filtrar Pokémons por nome através do input text
+export const filterByName = (pokemonsList, pokemonsName) => {
+  const lowerCaseName = pokemonsName.toLowerCase();
+  return pokemonsList
+    .filter((pokemon) => pokemon.name.toLowerCase().startsWith(lowerCaseName))
+    .slice(0, 251);
+};
+
+//Filtrar Pokémons por tipo através do select
+export const filterByType = (data, selectFilter) =>
+  data.filter((pokemon) => pokemon.type.includes(selectFilter));
+
+//Ordenar Pokémons por CP Max->Min/Min->Max
+export const orderCP = (data, selectOrder) => {
+  if (selectOrder === "MaxCPMinCP") {
+    return data.sort((maxcp, mincp) => mincp.stats["max-cp"] - maxcp.stats["max-cp"]
+    );
+  }  else  {
+    return data.sort((maxcp, mincp) => maxcp.stats["max-cp"] - mincp.stats["max-cp"]
+    );
+  }
+};
+
+//Cálculo
+export const percentType = (data, filterCalc) => {
+  const calcPoke = data.filter((pokemon) => pokemon.type.includes(filterCalc));
+  return Math.round((calcPoke.length / data.length) * 100);
+};*/
